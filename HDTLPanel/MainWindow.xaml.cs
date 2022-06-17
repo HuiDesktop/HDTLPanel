c780d5c0a24e9cacf3a262315e9a2f03b3b5515f
Pure01Fx <pure01fx@outlook.com>
2022-06-17 14:59:14 +0800

    feat: Simply imp launcher

--- HDTLPanel/MainWindow.xaml.cs
+++ HDTLPanel/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,115 @@ namespace HDTLPanel
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly MainWindowDataContext context = new();
+        ProcessManager? manager;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = context;
+        }
+
+        async private void SwitchSubprogramRunningStatus(object sender, RoutedEventArgs e)
+        {
+            if (context.IsRunning)
+            {
+                if (manager is not null)
+                {
+                    context.IsBusyClosing = true;
+                    manager.TryCloseWindow();
+                    await Task.Delay(1000);
+                    if (context.IsRunning)
+                    {
+                        manager.ForceCloseWindow();
+                    }
+                    manager = null;
+                    context.IsBusyClosing = false;
+                }
+            }
+            else
+            {
+                context.IsRunning = true;
+                manager = new ProcessManager("app/luajit.exe", System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app"), "main.lua");
+                manager.Exited += (_, _) =>
+                {
+                    context.IsRunning = false;
+                };
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (manager is not null)
+            {
+                manager.TryCloseWindow();
+                Thread.Sleep(2000);
+                if (context.IsRunning)
+                {
+                    manager.ForceCloseWindow();
+                }
+            }
+        }
+    }
+
+    class MainWindowDataContext : INotifyPropertyChanged
+    {
+        private bool isRunning = false;
+        private bool isBusyClosing = false;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool IsRunning
+        {
+            get => isRunning;
+            set
+            {
+                isRunning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsBusyClosing
+        {
+            get => isBusyClosing;
+            set
+            {
+                isBusyClosing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class BoolToStringValueConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool v && parameter is string s) return s.Split('/')[v ? 1 : 0];
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class ReverseBooleanValueConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool v) return !v;
+            return null;
+        }
+
+        public object? ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
         }
     }
 }
---
+++ HDTLPanel/ProcessManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDTLPanel
+{
+    internal class ProcessManager
+    {
+        public readonly Process process;
+        public event EventHandler? Exited;
+
+        public ProcessManager(string exeName, string workingDirectory, string arguments)
+        {
+            ProcessStartInfo processStartInfo = new(exeName, arguments) {
+                WorkingDirectory = workingDirectory,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+            process = Process.Start(processStartInfo) ?? throw new Exception("Failed to start process");
+            process.Exited += Process_Exited;
+            process.EnableRaisingEvents = true;
+        }
+
+        public void TryCloseWindow()
+        {
+            process.CloseMainWindow();
+        }
+
+        public void ForceCloseWindow()
+        {
+            process.Kill(true);
+        }
+
+        private void Process_Exited(object? sender, EventArgs e)
+        {
+            Exited?.Invoke(this, e);
+        }
+    }
+}
